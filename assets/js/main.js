0a46a94b7b7248e50b3aeb989c0f39ddbf80b0d1
Daniel Kim <daniel.d.kim13@gmail.com>
2022-01-02 22:00:36 -0500

    localstorage thang

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,10 @@ function cityNameInputHandler(e) {
   // conditional to check if the input is valid or not.
   if (userCityName) {
     console.log(userCityName);
+    savedSearch(userCityName);
+    instantDisplay(userCityName);
+    currentDayWeather(userCityName);
+
     // clear out the input value after user submit
     $("#cityName").val("");
   } else if (userCityName === null) {
@@ -17,3 +21,18 @@ function cityNameInputHandler(e) {
 }
 
 $("#cityForm").submit(cityNameInputHandler);
+
+// search history to localStorage
+let citySearchArry = JSON.parse(localStorage.getItem("city")) || [];
+
+function savedSearch(searchCity) {
+  // variable initializing the local storage to receive obj
+  const loc = "location"; //placeholder for right now only
+  const cityObj = {
+    city: searchCity,
+    location: loc,
+  };
+
+  citySearchArry.push(cityObj);
+  localStorage.setItem("city", JSON.stringify(citySearchArry));
+}
