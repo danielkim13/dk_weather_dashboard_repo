d0754d015cf41b6929fee817d0ad4cf12e8bb24c
Daniel Kim <daniel.d.kim13@gmail.com>
2022-01-07 21:30:02 -0500

    trying to fix bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@ function displayHistoricalSearch() {
     $(".history-list").click(function (event) {
       const cityHistory = event.target.textContent;
       // console.log(cityHistory);
-      currentDayWeather(cityHistory);
+      executeHistory(cityHistory); //! param is data not the city.. hmmm
     });
   });
 }
@@ -60,8 +60,28 @@ ul element */
 function instantDisplay(inputValue) {
   $(".list-wrapper").append("<p class='history-list p-2'>" + inputValue);
   $(".history-list").click(() => {
-    currentDayWeather(inputValue);
+    executeHistory(inputValue); // ! param is data not the city...
   });
+}
+
+// intermediate fetch for history search line. because I'm calling the savedsearch and instantdiplay functions after the response comes back. had to write this bridge function.
+function executeHistory(cityName) {
+  const appID = "330db953764b679cb99918f065ab10a8";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + appID;
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          currentForecast(data);
+        });
+      } else {
+        alert("Error: seems like the history search isn't working properly");
+      }
+    })
+    .catch((error) => {
+      alert("Unable to connect to the API");
+    });
 }
 
 // fetch weather API -- query for city name and imperial metrics
